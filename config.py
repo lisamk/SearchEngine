45887af44c9d711930f74b2773b113f356ff4bc3
Unknown <franz.strasser94@icloud.com>
2017-12-20 10:06:54 +0100

    Added description to config file

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-data_path = '/Volumes/Daten/temp/MultimediaSAR/div-2014'
+'''
+Config file for user specific paths and variables
+Usage:
+    first define any of these variables like the ones below
+    then import the file where you need the variables: "import config" and
+    finally use the variables like this: "config.data_path"
+'''
+data_path = '/Volumes/Daten/temp/MultimediaSAR/div-2014/devset/'
 feature_path = 'descvis/img/'
 ground_truth_path = 'gt/dGT/'
