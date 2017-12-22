a8d48d15041fd583217a226972c222e781bd5672
Unknown <franz.strasser94@icloud.com>
2017-12-22 20:05:24 +0100

    Task 2: Montecarlo Search
    - Updated clustering.py to be callable as function which takes all hyperparameters.
    - Created Montecarlo.py which does a Hyperparametersearch with random values (not finished yet).
    - clustering.py has still some problems with error cases and throws an exception in nearly half of the calls.

---
+++ task2/Montecarlo.py
@@ -0,0 +1,270 @@
+import random as rand
+import bitarray as ba
+import clustering
+
+'''
+Searching parameters
+'''
+TRIES = 10
+
+'''
+Hyperparameters
+- Features (any combination of the 10 features)
+- Clustering Algorithm (preselected to Agglomerative, DBSCAN and Spectral)
+- Settings for Clustering Algorithm:
+  - Agglomerative
+    - n_clusters (int > 1)
+    - affinity ('euclidean', 'l1', 'l2', 'manhattan', 'cosine', or 'precomputed')
+    - compute_full_tree fixed to True, because n_clusters will be much smaller than n_samples
+    - linkage ('ward', 'complete', or 'average', attention: in case of 'ward' affinity is fixed to 'euclidean')
+  - Spectral
+    - n_clusters (int > 1)
+    - eigen_solver (None, 'arpack', 'lobpcg', or 'amg', attention: 'amg' needs pyamg installed)
+    - random_state fixed to None, because we assume that the random seed value will not influence the results that much
+    - n_init (int) take a very close look on that!!!
+    - gamma (float, attention: is ignored for affinity='nearest_neighbors')
+    - affinity ('nearest_neighbors', 'precomputed', or 'rbf')
+    - n_neighbors (int, attention: is ignored for affinity='rbf')
+    - eigen_tol (float, only if eigen_solver='arpack')
+    - assign_labels ('kmeans', or 'discretize')
+  - DBSCAN
+    - eps (float)
+    - min_samples (int > 1)
+    - algorithm ('auto', 'ball_tree', 'kd_tree', or 'brute')
+    - p (float)
+'''
+
+print('Start montecarlo search with ' + str(TRIES) + ' tries...')
+
+bestMin = {'value': 1.0, 'cnt': -1, 'params': None}
+bestMean = {'value': 0.0, 'cnt': -1, 'params': None}
+bestSD = {'value': 1.0, 'cnt': -1, 'params': None}
+bestMedian = {'value': 0.0, 'cnt': -1, 'params': None}
+bestMax = {'value': 0.0, 'cnt': -1, 'params': None}
+
+cnts = [0, 0, 0]
+first_loop = True
+
+for cnt in range(TRIES):
+    '''
+    features_map
+      BitArray that represents the chosen features
+      From MSB to LSB these features are described:
+        0 CM
+        1 CM3x3
+        2 CN
+        3 CN3x3
+        4 CSD
+        5 GLRLM
+        6 GLRLM3x3
+        7 HOG
+        8 LBP
+        9 LBP3x3
+      use random number with 10 Bit (unsigned int from 1 to 1023)
+    '''
+    features_map = ba.bitarray(('{0:010b}'.format(rand.randint(1, 1023)))[-10:])
+
+    '''
+    clustering_algorithm
+      integer that represents the chosen clustering algorithm
+      These 3 preselected algorithms are available:
+        1 AgglomerativeClustering
+        2 SpectralClustering
+        3 DBSCAN
+    '''
+    clustering_algorithm = rand.randint(1, 3)
+
+    '''
+    n_clusters
+      integer that represents the number of clusters to produce.
+      This is only applied, in case of Agglomerative or Spectral clustering.
+
+      Decisions for limits:
+      1. Init: limit from 10 to 50
+      2.
+    '''
+    n_clusters = rand.randint(10, 50)
+
+    '''
+    linkage
+      string that describes which linkage criterion to use
+    '''
+    linkage_values = ['ward', 'complete', 'average']
+    linkage = linkage_values[rand.randint(0, len(linkage_values)-1)]
+
+    '''
+    affinity
+      string that defines the metric used to compute the linkage
+      This is only applied in case of Agglomerative or Spectral clustering
+      AglomerativeClustering values:
+        'euclidean', 'l1', 'l2', 'manhattan', 'cosine', or 'precomputed'
+      SpectralClustering values:
+        'nearest_neighbors', 'precomputed', or 'rbf'
+    '''
+    affinity = None
+    agglomerative_affinity_values = ['euclidean', 'l1', 'l2', 'manhattan', 'cosine', 'precomputed']
+    spectral_affinity_values = ['nearest_neighbors', 'precomputed', 'rbf']
+    if clustering_algorithm == 1:
+        # Agglomerative Clustering
+        if linkage == 'ward':
+            affinity = 'euclidean'
+        else:
+            affinity = agglomerative_affinity_values[rand.randint(0, len(agglomerative_affinity_values) - 1)]
+    if clustering_algorithm == 2:
+        # Spectral Clustering
+        affinity = spectral_affinity_values[rand.randint(0, len(spectral_affinity_values) - 1)]
+
+    '''
+    compute_full_tree
+      stops early construction of tree at n_cluster
+      can be helpful regarding performance to set to False if n_cluster is in same dimension than n_samples
+      => we fix it to True
+    '''
+    compute_full_tree = True
+
+    '''
+    eigen_solver
+      string that describes the eigenvalue decomposition strategy
+    '''
+    eigen_solver_values = [None, 'arpack', 'lobpcg', 'amg']
+    eigen_solver = eigen_solver_values[rand.randint(0, len(eigen_solver_values)-1)]
+
+    '''
+    random_state
+      can be used to define a random number generator in some eigen_solver cases
+      we do not use this => None
+    '''
+    random_state = None
+
+    '''
+    n_init
+      int that defines the number of times a k-means algorithm will run with different seeds
+
+      Decisions for limits:
+      1. Init: limit from 5 to 20, because default is 10
+      2.
+    '''
+    n_init = rand.randint(5, 20)
+
+    '''
+    gamma
+      float that describes the kernel coefficient.
+      is not used in case of 'nearest-neighbor' affinity
+
+      Decisions for limits:
+      1. Init: limit from 0.0 to 1.0, because no prior knowledge
+      2.
+    '''
+    if affinity == 'nearest-neighbor':
+        gamma = None
+    else:
+        gamma = rand.uniform(0.0, 1.0)
+
+    '''
+    n_neighbors
+      int that describes number of neighbors to use when constructing the affinity matrix
+
+      Decisions for limits:
+      1. Init: limit from 3 to 50, because no prior knowledge
+      2.
+    '''
+    if affinity == 'rbf':
+        n_neighbors = None
+    else:
+        n_neighbors = rand.randint(3, 50)
+
+    '''
+    eigen_tol
+      float that describes the stopping criterion for eigendecomposition
+
+      Decisions for limits:
+      1. Init: limit from 0.0 to 1.0, because no prior knowledge
+      2.
+    '''
+    eigen_tol = rand.uniform(0.0, 1.0)
+
+    '''
+    assign_labels
+      string that describes the strategy to use by assigning labels in the embedding space
+    '''
+    assign_labels_values = ['kmeans', 'discretize']
+    assign_labels = assign_labels_values[rand.randint(0, len(assign_labels_values)-1)]
+
+    '''
+    eps
+      float that describes the maximum distance between two values to be considered as in the same neighborhood
+
+      Decisions for limits:
+      1. Init: limit from 0.0 to 1.0, because no prior knowledge
+      2.
+    '''
+    eps = rand.uniform(0.0, 1.0)
+
+    '''
+    min_samples
+      int that defines the number of samples in a neighborhood for a point to be considered as a core point
+
+      Decisions for limits:
+      1. Init: limit from 3 to 50, because no prior knowledge
+      2.
+    '''
+    min_samples = rand.randint(3, 50)
+
+    '''
+    algorithm
+      string that describes the method used for calculating the distance between points
+    '''
+    algorithm_values = ['auto', 'ball_tree', 'kd_tree', 'brute']
+    algorithm = algorithm_values[rand.randint(0, len(algorithm_values)-1)]
+
+    '''
+    p
+      float that describes the power of the Minkowski metric
+
+      Decisions for limits:
+      1. Init: limit from 0.0 to 1.0, because no prior knowledge
+      2.
+    '''
+    p = rand.uniform(0.0, 1.0)
+
+    if first_loop:
+        print("Min\tMean\tSD\tMedian\tMax")
+        first_loop = False
+
+    cnts[clustering_algorithm-1] += 1
+    try:
+        result = clustering.cluster(features_map=features_map, clustering_algorithm=clustering_algorithm, n_clusters=n_clusters,
+                                    linkage=linkage, affinity=affinity, compute_full_tree=compute_full_tree,
+                                    eigen_solver=eigen_solver, random_state=random_state, n_init=n_init, gamma=gamma,
+                                    n_neighbors=n_neighbors, eigen_tol=eigen_tol, assign_labels=assign_labels, eps=eps,
+                                    min_samples=min_samples, algorithm=algorithm, p=p)
+        print(str(result['min'])+"\t"+str(result['mean'])+"\t"+str(result['sd'])+"\t"+str(result['median'])+"\t"+str(result['max']))
+    except Exception as ex:
+        clustering_algorithm_values = ['Agglomerative', 'Spectral', 'DBSCAN']
+        print("\n\nERROR:")
+        print(type(ex))
+        print(ex)
+        print("\nParams")
+        print("features_map="+str(features_map))
+        print("clustering_algorithm="+str(clustering_algorithm_values[clustering_algorithm]))
+        print("n_clusters="+str(n_clusters))
+        print("linkage="+str(linkage))
+        print("affinity="+str(affinity))
+        print("compute_full_tree="+str(compute_full_tree))
+        print("eigen_solver="+str(eigen_solver))
+        print("random_state="+str(random_state))
+        print("n_init="+str(n_init))
+        print("gamma="+str(gamma))
+        print("n_neighbors="+str(n_neighbors))
+        print("eigen_tol="+str(eigen_tol))
+        print("assign_labels="+str(assign_labels))
+        print("eps="+str(eps))
+        print("min_samples="+str(min_samples))
+        print("algorithm="+str(algorithm))
+        print("p="+str(p))
+        print("\n\n")
+
+print("\n\nAuswertung:")
+print(str(cnts[0]) + " Agglomerative")
+print(str(cnts[1]) + " Spectral")
+print(str(cnts[2]) + " DBSCAN")
--- task2/clustering.py
+++ task2/clustering.py
@@ -1,8 +1,12 @@
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 from sklearn.cluster import DBSCAN
 import pandas as pd
 import numpy as np
 import statistics as st
+import bitarray as ba
 import config
+import time
 
 '''
 This is a first start of task 2
@@ -35,66 +39,123 @@ DATA_DIR = config.data_path
 FEATURE_PATH = config.feature_path
 GROUND_TRUTH_PATH = config.ground_truth_path
 
-# do clustering for every location we have in the dev set
-# read locations file
-df_locations = pd.read_csv(DATA_DIR + "poiNameCorrespondences.txt", sep="\t", header=None)
-# remove first column (names)
-locations = np.array(df_locations[1])
+def cluster(features_map, clustering_algorithm, n_clusters, linkage, affinity, eigen_solver, n_init, gamma, n_neighbors, eigen_tol, assign_labels, eps, min_samples, algorithm, p, compute_full_tree = True, random_state = None) :
+    # do clustering for every location we have in the dev set
+    # read locations file
+    df_locations = pd.read_csv(DATA_DIR + "poiNameCorrespondences.txt", sep="\t", header=None)
+    # remove first column (names)
+    locations = np.array(df_locations[1])
 
-score = []
-for location in locations:
-    # read in the file with the list of clusters in athens
-    df_cluster = pd.read_csv(DATA_DIR + GROUND_TRUTH_PATH + location + " dclusterGT.txt", sep=",", header=None)
-    # we only need the number of clusters for now
-    n_clusters = len(df_cluster)
+    score = []
+    firstLoop = True
+    for location in locations:
+        # read the ground truth file for the images
+        df_gt = pd.read_csv(DATA_DIR + GROUND_TRUTH_PATH + location + " dGT.txt", sep=",", header=None)
+        # create a dictionary of the form { imageID : clusterID }
+        truth = dict(zip(df_gt[0], df_gt[1]))
 
-    # read the ground truth file for the images
-    df_gt = pd.read_csv(DATA_DIR + GROUND_TRUTH_PATH + location + " dGT.txt", sep=",", header=None)
-    # create a dictionary of the form { imageID : clusterID }
-    truth = dict(zip(df_gt[0], df_gt[1]))
+        # read in the image features
+        features_df = []
+        # copy features map for every loop
+        tmp_features_map = features_map
+        if tmp_features_map >= ba.bitarray('1000000000'):
+            # CM
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CM.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0111111111')
+        if tmp_features_map >= ba.bitarray('0100000000'):
+            # CM3x3
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CM3x3.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0011111111')
+        if tmp_features_map >= ba.bitarray('0010000000'):
+            # CN
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CN.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0001111111')
+        if tmp_features_map >= ba.bitarray('0001000000'):
+            # CN3x3
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CN3x3.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000111111')
+        if tmp_features_map >= ba.bitarray('0000100000'):
+            # CSD
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CSD.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000011111')
+        if tmp_features_map >= ba.bitarray('0000010000'):
+            # GLRLM
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " GLRLM.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000001111')
+        if tmp_features_map >= ba.bitarray('0000001000'):
+            # GLRLM3x3
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " GLRLM3x3.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000000111')
+        if tmp_features_map >= ba.bitarray('0000000100'):
+            # HOG
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " HOG.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000000011')
+        if tmp_features_map >= ba.bitarray('0000000010'):
+            # LBP
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " LBP.csv", sep=",", header=None))
+            tmp_features_map = tmp_features_map & ba.bitarray('0000000001')
+        if tmp_features_map >= ba.bitarray('0000000001'):
+            # LBP3x3
+            features_df.append(pd.read_csv(DATA_DIR + FEATURE_PATH + location + " LBP3x3.csv", sep=",", header=None))
 
-    # read in the image features
-    df_feature = pd.read_csv(DATA_DIR + FEATURE_PATH + location + " CM.csv", sep=",", header=None)
-    # TODO add more features into the array here
-    # read the ids into an array
-    ids = np.array(df_feature[0])
+        # read the ids into an array
+        ids = np.array(features_df[0][0])
+        # start with id column and remove later
+        #features = ids
+        first = True
+        for df_feature in features_df:
+            # remove the first column with the image ids
+            df_feature = df_feature.drop([0], axis=1)
+            # create an array of all feautures [id,f1,f2,....]
+            if first:
+                features = np.array(df_feature)
+            else:
+                features = np.concatenate((features, np.array(df_feature)), axis=1)
+            first = False
+        # now remove the initially added ids
+        #features = features[1:]
 
-    # remove the first column with the image ids
-    df_feature = df_feature.drop([0], axis=1)
-    # create an array of the feautures [f1,f2,....]
-    features = np.array(df_feature)
+        # use feature array and number of clusters from above
+        # use DBSCAN because it does not need the number of clusters
+        if clustering_algorithm < 1:
+            print("\n\nInvalid clustering algorithm: " + clustering_algorithm + "!\n\n")
+            return
+        if clustering_algorithm == 1:
+            model = AgglomerativeClustering(n_clusters=n_clusters, affinity=affinity, compute_full_tree=compute_full_tree, linkage=linkage)
+        if clustering_algorithm == 2:
+            model = SpectralClustering(n_clusters=n_clusters, eigen_solver=eigen_solver, random_state=random_state, n_init=n_init, gamma=gamma, affinity=affinity, n_neighbors=n_neighbors, eigen_tol=eigen_tol, assign_labels=assign_labels, n_jobs=-1)
+        if clustering_algorithm == 3:
+            model = DBSCAN(eps=eps, min_samples=min_samples, algorithm=algorithm, p=p, n_jobs=-1)
+        if clustering_algorithm > 3:
+            print("\n\nInvalid clustering algorithm: " + clustering_algorithm + "!\n\n")
+            return
+        model.fit(features)
+        # create dictionary { imageID, predictedCluster }
+        prediction = dict(zip(ids, model.labels_))
 
-    # use feature array and number of clusters from above
-    # use DBSCAN because it does not need the number of clusters
-    model = DBSCAN().fit(features)
-    # create dictionary { imageID, predictedCluster }
-    prediction = dict(zip(ids, model.labels_))
+        # there isn't a ground truth for each image, so we can use the subset for comparision
+        # additionally the predictions are now in the same order as the truth values
+        prediction_subset = {x: prediction[x] for x in truth.keys() if x in prediction}
 
-    # there isn't a ground truth for each image, so we can use the subset for comparision
-    # additionally the predictions are now in the same order as the truth values
-    prediction_subset = {x: prediction[x] for x in truth.keys() if x in prediction}
+        # in the next step we pairwisely compare each key with each other
+        # if they are in the same cluster in the each dictionary
+        correct = 0
+        wrong = 0
+        for key1 in prediction_subset.keys():
+            for key2 in prediction_subset.keys():
+                if key1 != key2:
+                    if ((truth.get(key1) == truth.get(key2) and (
+                                prediction_subset.get(key1) == prediction_subset.get(key2))) or (
+                                    truth.get(key1) != truth.get(key2) and (
+                                        prediction_subset.get(key1) != prediction_subset.get(key2)))):
+                        correct += 1
+                    else:
+                        wrong += 1
 
-    # in the next step we pairwisely compare each key with each other
-    # if they are in the same cluster in the each dictionary
-    correct = 0
-    wrong = 0
-    for key1 in prediction_subset.keys():
-        for key2 in prediction_subset.keys():
-            if key1 != key2:
-                if ((truth.get(key1) == truth.get(key2) and (
-                            prediction_subset.get(key1) == prediction_subset.get(key2))) or (
-                                truth.get(key1) != truth.get(key2) and (
-                                    prediction_subset.get(key1) != prediction_subset.get(key2)))):
-                    correct += 1
-                else:
-                    wrong += 1
+        # the performance is measured by Rand index
+        # https://en.wikipedia.org/wiki/Rand_index
+        score.append(correct / (correct + wrong))
 
-    # the performance is measured by Rand index
-    # https://en.wikipedia.org/wiki/Rand_index
-    score.append(correct / (correct + wrong))
-
-# calculate statistics over all scores
-print("Min:    " + str(min(score)))
-print("Mean:   " + str(sum(score) / len(score)))
-print("Median: " + str(st.median(score)))
-print("Max:    " + str(max(score)))
+    # calculate statistics over all scores
+    #print(str(min(score))+"\t"+(str(sum(score)/len(score)))+"\t"+str(np.std(score))+"\t"+str(st.median(score))+"\t"+str(max(score)))
+    return {'min': min(score), 'mean': (sum(score)/len(score)), 'sd': np.std(score), 'median': st.median(score), 'max': max(score)}
