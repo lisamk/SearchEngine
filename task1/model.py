843017d7ced14d11edac82cb37f59f1f810dc94d
Lisa <kritzinger@gmx.net>
2017-12-29 14:02:06 +0100

    model added

---
+++ task1/model.py
@@ -0,0 +1,16 @@
+class model:
+
+    def __init__(self, file):
+        self.file = file;
+
+    def setFileNameScore(self, score):
+        self.fileNameScore = score
+
+    def setDistanceScore(self, score):
+        self.distanceScore = score
+
+    def setViewsScore(self, score):
+        self.viewsScore = score
+
+    def setTagsScore(self, score):
+        self.tagsScore(self, score)
--- task1/search.py
+++ task1/search.py
@@ -3,36 +3,46 @@ from math import radians, cos, sin, asin, sqrt
 from geopy.geocoders import Nominatim
 import os
 import zipfile
+import config
+import model
 
 class searchMethod:
 
-    def filter(search):
+    def rate(search):
 
-        def searchFor(word):
+        def rateFileName(word):
             relevantFilesList = []
-            relevantLines = []
+            files = []
 
-            with zipfile.ZipFile('E:/Users/Gudrun/Documents/JKU/WS1718/Multimedia Search/div-2014/devset/desccred.zip') as z:
+
+            # 1 if in file name, else 0
+            with zipfile.ZipFile(config.data_path+'/desccred.zip') as z:
                 for filename in z.namelist():
                   if not os.path.isdir(filename):
                         with z.open(filename, 'r') as f:
                             for line in f:
-                                if word in line:
-                                    if filename not in relevantFilesList:
-                                        relevantFilesList.append(filename)
-                                        relevantLines.append(line)
+                                m = model(line)
+                                files.append(m)
+                                if filename not in relevantFilesList:
+                                    relevantFilesList.append(filename)
+                                    if word in line:
+                                        m.setFileNameScore(1)
+                                    else:
+                                        m.setFileNameScore(0)
+                                       # relevantLines.append(line)
 
 
-            for name in relevantFilesList[:]:
-                f = zipfile.ZipFile('E:/Users/Gudrun/Documents/JKU/WS1718/Multimedia Search/div-2014/devset/desccred.zip')
-                xmlfile = f.open(name)
-                xmldoc = minidom.parse(xmlfile)
-                idlist = xmldoc.getElementsByTagName('photo')
+            # for name in relevantFilesList[:]:
+            #     f = zipfile.ZipFile(config.data_path+'/desccred.zip')
+            #     xmlfile = f.open(name)
+            #     xmldoc = minidom.parse(xmlfile)
+            #     idlist = xmldoc.getElementsByTagName('photo')
 
-            return relevantLines
+            return files
 
-        def filterByCoordinates(relevant, search):
-            relevantLines = []
+        # 1-Entfernung/(6371/2)
+        def rateByCoordinates(model, search):
+            r = 6371  # Radius of earth in kilometers. Use 3956 for miles
 
             def haversine(lon2, lat2):
                 # convert decimal degrees to radians
@@ -46,62 +56,61 @@ class searchMethod:
                 dlat = lat2 - lat1
                 a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
                 c = 2 * asin(sqrt(a))
-                r = 6371  # Radius of earth in kilometers. Use 3956 for miles
                 return c * r
 
-            for line in relevant:
-                xml = minidom.parseString(line)
+            for m in model:
+                xml = minidom.parseString(m.file)
                 photo = xml.getElementsByTagName("photo")[0]
                 if photo.hasAttribute('latitude') and photo.hasAttribute('longitude'):
                     lat2 = float(photo.getAttribute('latitude'))
                     lon2 = float(photo.getAttribute('longitude'))
-                    radius = 10.00  #TODO
+                  #  radius = 10.00
                     a = haversine(lon2, lat2)
-                    if a <= radius:
-                        relevantLines.append(line)
+                    m.setDistanceScore(1-(r/2)/a)
                 else:
-                    relevantLines.append(line)
+                    m.setDistanceScore(0) #TODO
 
-            return relevantLines
+            return model
 
-        def filterByTags(relevant):
-            relevantLines = []
+        # 1-foundTags/numTags
+        def rateByTags(model):
             unrelevant = ["karneval","market"] #TODO find more
 
-            for line in relevant:
-                xml = minidom.parseString(line)
+            for m in model:
+                count = 0
+                xml = minidom.parseString(m.file)
                 photo = xml.getElementsByTagName("photo")[0]
                 if photo.hasAttribute('tags'):
                     tags = photo.getAttribute('tags')
-                    relevant = True
                     for u in unrelevant:
                         if u in tags:
-                            unrelevant = False
-                            break
-                    if relevant:
-                        relevantLines.append(line)
-            return relevantLines
+                            count = count + 1
+                    m.setTagsScore(1-count/len(unrelevant))
+            return m
 
-        'TODO'
-        def filterByFlickr(relevant):
-            relevantLines = []
-            for line in relevant:
-                xml = minidom.parseString(line)
+        # 1-views/maxviews
+        def rateByViews(model):
+            maxViews = 0
+            for m in model:
+                xml = minidom.parseString(m.file)
                 photo = xml.getElementsByTagName("photo")[0]
                 if photo.hasAttribute('views'):
                     views = int(photo.getAttribute('views'))
-                    if views > 100:
-                        relevantLines.append(line)
-            return relevantLines
+                    if views>maxViews:
+                        maxViews = views
+            for m in model:
+                xml = minidom.parseString(m.file)
+                photo = xml.getElementsByTagName("photo")[0]
+                if photo.hasAttribute('views'):
+                    views = int(photo.getAttribute('views'))
+                    m.setViewsScore(1-views/maxViews)
+            return model
 
-        filteredByName = searchFor(search)
-     #   filteredByName = ["""<photo date_taken="2010-09-04 19:14:52" id="5004570811" tags="panorama toronto montreal niagrafalls sydney geiranger" title="Geiranger, Norway" url_b="http://farm5.static.flickr.com/4106/5004570811_0a860b6b68_b.jpg" userid="89093444@N00" views="262" />""",
-     #                     """<photo date_taken="2005-02-11 14:27:52" id="4614474" latitude="-33.862919" longitude="151.212601" tags="hotel honeymoon sydney australia" title="Sydney:  Hotel Intercontinental" url_b="http://farm1.static.flickr.com/5/4614474_793ccae3d0_b.jpg" userid="89504146@N00" views="139" />""",
-     #                     """<photo date_taken="2005-08-23 09:24:06" id="36456603" tags="sky clouds town sydney australia hasselblad" title="Sydney" url_b="http://farm1.static.flickr.com/30/36456603_9de5c110ef_b.jpg" userid="89826095@N00" views="56" />"""]
-        filteredByLocation = filterByCoordinates(filteredByName, search)
-        filteredByTags = filterByTags(filteredByLocation)
-        return filterByFlickr(filteredByTags);
+        rated = rateFileName(search)
+     #   rated = rateByCoordinates(rated, search)
+     #   rated = rateByTags(rated)
+     #   return rateByViews(rated);
 
-    result = filter("norway")
+    result = rate("norway")
     for s in result:
-        print(s);
+        print(s.file);
