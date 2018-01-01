e2908124a28699da156ac842270556a312ff03af
Lisa <kritzinger@gmx.net>
2018-01-01 17:13:17 +0100

    optimizaion

--- task1/search.py
+++ task1/search.py
@@ -20,24 +20,14 @@ class rate_method:
 
     def rate(self, search):
 
-        def rate_name():
-
-            # 1 if in file name, else 0
-            with zipfile.ZipFile(config.data_path+'testG.zip') as z:
-                for filename in z.namelist():
-                  if not os.path.isdir(filename):
-                        with z.open(filename, 'r') as f:
-                            for line in f:
-                                if "<photo ".encode() in line:
-                                    line = line.decode('utf-8')
-                                    self.files.append(line)
-                                    if search.encode().decode('utf-8').upper() in line.upper():
-                                        self.name_score.append(1)
-                                    else:
-                                        self.name_score.append(0)
+        def rate_name(line):
+            if search.encode().decode('utf-8').upper() in line.upper():
+                self.name_score.append(1)
+            else:
+                self.name_score.append(0)
 
         # 1-Entfernung/(6371/2)
-        def rate_distance():
+        def rate_distance(line):
             r = 6371  # Radius of earth in kilometers. Use 3956 for miles
 
             def haversine(lon2, lat2):
@@ -54,55 +44,32 @@ class rate_method:
                 c = 2 * asin(sqrt(a))
                 return c * r
 
-            for line in self.files:
-                xml = minidom.parseString(line)
-                photo = xml.getElementsByTagName("photo")[0]
-                if photo.hasAttribute('latitude') and photo.hasAttribute('longitude'):
-                    lat2 = float(photo.getAttribute('latitude'))
-                    lon2 = float(photo.getAttribute('longitude'))
-                  #  radius = 10.00
-                    a = haversine(lon2, lat2)
-                    self.distance_score.append(1-(r/2)/a)
-                else:
-                    self.distance_score.append(0.5) #TODO to discuss
+            xml = minidom.parseString(line)
+            photo = xml.getElementsByTagName("photo")[0]
+            if photo.hasAttribute('latitude') and photo.hasAttribute('longitude'):
+                lat2 = float(photo.getAttribute('latitude'))
+                lon2 = float(photo.getAttribute('longitude'))
+                a = haversine(lon2, lat2)
+                self.distance_score.append(1-(r/2)/a)
+            else:
+                self.distance_score.append(0.5) #TODO to discuss
 
         # 1-foundTags/numTags
-        def rate_tags():
-            unrelevant = ["karneval","market","riot","protest","demonstration"] #TODO find more
+        def rate_tags(line):
+            unrelevant = ["karneval", "market", "riot", "protest", "demonstration"]  # TODO find more
+            count = 0
+            xml = minidom.parseString(line.encode('utf-8'))
+            photo = xml.getElementsByTagName("photo")[0]
+            if photo.hasAttribute('tags'):
+                tags = photo.getAttribute('tags')
+                for u in unrelevant:
+                    if u in tags:
+                        count = count + 1
+                        self.tag_score.append(1 - count / len(unrelevant))
+                    else:
+                        self.tag_score.append(0.5)
 
-            for line in self.files:
-                count = 0
-                xml = minidom.parseString(line.encode('utf-8'))
-                photo = xml.getElementsByTagName("photo")[0]
-                if photo.hasAttribute('tags'):
-                    tags = photo.getAttribute('tags')
-                    for u in unrelevant:
-                        if u in tags:
-                            count = count + 1
-                    self.tag_score.append(1-count/len(unrelevant))
-                else:
-                    self.tag_score.append(0.5)
-
-        # 1-views/maxviews
-        def rate_views():
-            maxViews = 0
-            for line in self.files:
-                xml = minidom.parseString(line.encode('utf-8'))
-                photo = xml.getElementsByTagName("photo")[0]
-                if photo.hasAttribute('views'):
-                    views = int(photo.getAttribute('views'))
-                    if views>maxViews:
-                        maxViews = views
-            for line in self.files:
-                xml = minidom.parseString(line.encode('utf-8'))
-                photo = xml.getElementsByTagName("photo")[0]
-                if photo.hasAttribute('views'):
-                    views = int(photo.getAttribute('views'))
-                    self.views_score.append(1-views/maxViews)
-                else:
-                    self.views_score.append(0.5)
-
-        def rate_face():
+        def rate_face(line):
             def checkForFaces(url):
                 resp = urllib.urlopen(url)
                 image = np.array(bytearray(resp.read()), dtype="uint8")
@@ -115,33 +82,51 @@ class rate_method:
                 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.03,
                                                       minNeighbors=25, minSize=(70, 70),
                                                       flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
-                #for (x, y, w, h) in faces:
-                    #cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 if len(faces) > 0:
                     return True
                 else:
                     return False
-            for line in self.files:
-                xml = minidom.parseString(line.encode('utf-8'))
-                photo = xml.getElementsByTagName("photo")[0]
-                url = photo.getAttribute('url_b')
 
-                #for filename in os.listdir(folder):
-                 #   if not os.path.isdir(filename):
-                  #      if ".jpg" in filename and id in filename:
-                   #         tmp = checkForFaces(folder + filename)
+            xml = minidom.parseString(line.encode('utf-8'))
+            photo = xml.getElementsByTagName("photo")[0]
+            url = photo.getAttribute('url_b')
+            tmp = checkForFaces(url)
+            if tmp:
+                self.face_score.append(1)
+            else:
+                self.face_score.append(0)
 
-                tmp = checkForFaces(url)
-                if tmp:
-                   self.face_score.append(1)
-                else:
-                    self.face_score.append(0)
+        # 1 if in file name, else 0
+        max_view = 0
+        with zipfile.ZipFile(config.data_path+'desccred2.zip') as z:
+            for filename in z.namelist():
+                if not os.path.isdir(filename):
+                    with z.open(filename, 'r') as f:
+                        for line in f:
+                            if "<photo ".encode() in line:
+                                line = line.decode('utf-8')
+                                self.files.append(line)
+                                rate_name(line)
+                                rate_distance(line)
+                                rate_tags(line)
+                                rate_face(line)
 
-        rate_name()
-       # rate_distance()
-        rate_tags()
-        rate_views()
-        rate_face()
+                                xml = minidom.parseString(line.encode('utf-8'))
+                                photo = xml.getElementsByTagName("photo")[0]
+                                if photo.hasAttribute('views'):
+                                    views = int(photo.getAttribute('views'))
+                                    if views > maxViews:
+                                        maxViews = views
+        #rate views
+        for line in self.files:
+            xml = minidom.parseString(line.encode('utf-8'))
+            photo = xml.getElementsByTagName("photo")[0]
+            if photo.hasAttribute('views'):
+                views = int(photo.getAttribute('views'))
+                self.views_score.append(1-views/maxViews)
+            else:
+                self.views_score.append(0.5)
+
         for i in range(0, len(s.files)):
             self.images.append(image(s.files[i], s.name_score[i], 0, s.views_score[i], s.tag_score[i], s.face_score[i]))
 
